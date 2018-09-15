1f9c0d5db634c5d367b05a294a81c49782f9bbbb
Johan Wänglöf <jwanglof@gmail.com>
2018-09-15 08:34:36 +0200

    Removed Day-component

--- src/components/day/Day.test.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import Day from "./Day";
-//
-// describe("Day component", () => {
-//   it("hej", () => {
-//     const dayDate = ""
-//     const wrapper = shallow(<Day/>);
-//   });
-// });
